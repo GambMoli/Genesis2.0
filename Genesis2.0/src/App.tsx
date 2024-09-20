87326f3142c1f69feed23cd826f36d82cceaff40
GambMoli <g.molina@nelumbo.com.co>
2024-09-19 20:15:58 -0500

    feat: first commit

---
+++ Genesis2.0/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Login, Home } from './Modules';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ Genesis2.0/src/Modules/PrincipalModule/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Luis y rozo son rolo de pajuos</p>
+    </div>
+  );
+};
