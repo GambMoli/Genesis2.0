e6541b82a5c1b2736abc28252392cd9a9a0a77eb
RozoHyper <jstr234567@gmail.com>
2024-09-21 13:23:50 -0500

    fix:Header

--- src/Core/Components/Header/index.tsx
+++ src/Core/Components/Header/index.tsx
@@ -1,36 +1,45 @@
-import React from 'react'
-import { HomeFilled,CalendarFilled,BellOutlined,UserOutlined  } from '@ant-design/icons';
-import './StyleHeader.css'
-import Logo from '../../../assets/logo_udes.png'
-import  { Col, Row,Flex,Button } from 'antd';
+import React from 'react';
+import { HomeFilled, CalendarFilled, BellOutlined, UserOutlined } from '@ant-design/icons';
+import './StyleHeader.css';
+import Logo from '../../../assets/logo_udes.png';
+import { Col, Row, Button} from 'antd';
+
 import { useNavigate } from 'react-router-dom';
 
 export const Header = () => {
-  const navigate= useNavigate();
-  const handlenavigation=()=> {
-  navigate('../../../Modules/PrincipalModules/index')
-}
+  const navigate = useNavigate();
+  
+  const handleNavigation = () => {
+    navigate('../../../Modules/PrincipalModules/index');
+  }
+  
   return (
-    <div className='container'>
-      <img src={Logo} alt="Logo udes" className="LogoUdes" />
-      <Row className='navBar'>
-        <Flex gap={"large"} className='EspacioIzquierdo'>
-          <Col><Button className='BotonHome' onClick={handlenavigation}><HomeFilled className='iconHome' /></Button></Col>
-          <Col>Tramites academicos</Col>
-          <Col>Record de notas</Col>
-          <Col>Plan de estudio</Col>
-          <Col>Matrícula</Col>
-          <Col>Horario</Col>
-          <Col>Notas</Col>
-        </Flex>
-        <Flex gap={"large"}>
-          <Col>Tutoriales</Col>
-          <Col> <CalendarFilled className='iconCalendar' /></Col>
-          <Col><BellOutlined className='iconBell' /></Col>
-          <Col><UserOutlined className='iconUser' />Sesion de la persona</Col>
-        </Flex>
+    /*<div className='container'>*/
+      <Row className='container'>
+        <Col>
+          <img src={Logo} alt="Logo udes" className="LogoUdes" />
+        </Col>
+       
+        
+          <Row className='navBar' >
+            <Col className='EspacioIzquuierdo'>
+            <Col><Button className='BotonHome' onClick={handleNavigation}><HomeFilled className='iconHome' /></Button></Col>
+            <Col>Tramites academicos</Col>
+            <Col>Record de notas</Col>
+            <Col>Plan de estudio</Col>
+            <Col>Matrícula</Col>
+            <Col>Horario</Col>
+            <Col>Notas</Col>
+            </Col>
+            <Col className='EspacioDerecho'>
+            <Col>Tutoriales</Col>
+            <Col><CalendarFilled className='iconCalendar' /></Col>
+            <Col><BellOutlined className='iconBell' /></Col>
+            <Col><UserOutlined className='iconUser' />Sesion de la persona</Col>
+            </Col>
+          </Row>
+          
       </Row>
-
-    </div>
+    //</div>
   );
 }
