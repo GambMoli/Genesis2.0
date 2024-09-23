f156b0b71233d04895d75a4b1b33ef7961b5b88e
GambMoli <g.molina@nelumbo.com.co>
2024-09-22 21:06:15 -0500

    fix: fixing issues to build

--- src/Core/Components/TablaPrincipalPage/index.tsx
+++ src/Core/Components/TablaPrincipalPage/index.tsx
@@ -1,42 +1,41 @@
-import React from 'react';
 import './StyleTablaInformacion.css';
-import { Table  } from 'antd';
+import { Table } from 'antd';
 import { CheckOutlined } from '@ant-design/icons';
 
 export const TablaInformacion = () => {
-    const columns = [
-        {
-          title: "Concepto",
-          dataIndex: "concepto",
-          key: "concepto",
-        },
-        {
-          title: "Valor",
-          dataIndex: "valor",
-          key: "valor",
-        }
-      ];
-    
-      const data = [
-        {
-          key: 1,
-          concepto: "Promedio",
-          valor: 4.0,
-        },
-        {
-          key: 2,
-          concepto: "Semestre",
-          valor: 6,
-        },
-        {
-          key: 3,
-          concepto: "Activo académico",
-          valor: <CheckOutlined />,
-        }
-      ];
-    
+  const columns = [
+    {
+      title: "Concepto",
+      dataIndex: "concepto",
+      key: "concepto",
+    },
+    {
+      title: "Valor",
+      dataIndex: "valor",
+      key: "valor",
+    }
+  ];
+
+  const data = [
+    {
+      key: 1,
+      concepto: "Promedio",
+      valor: 4.0,
+    },
+    {
+      key: 2,
+      concepto: "Semestre",
+      valor: 6,
+    },
+    {
+      key: 3,
+      concepto: "Activo académico",
+      valor: <CheckOutlined />,
+    }
+  ];
+
   return (
-    
-          <Table columns={columns} dataSource={data} className="customTable" />
+
+    <Table columns={columns} dataSource={data} className="customTable" />
   );
 }
