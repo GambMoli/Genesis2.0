151a6f848230fccccbfe151cf82ba1673562dc92
RozoHyper <jstr234567@gmail.com>
2024-09-23 16:44:13 -0500

    Feat:Realizacion del footer

--- src/Core/Components/Footer/index.tsx
+++ src/Core/Components/Footer/index.tsx
@@ -1,13 +1,20 @@
 
 import './FooterStyle.css'
-
+import { Typography } from 'antd';
+const { Paragraph } = Typography;
 export const Footer = () => {
     return (
-<footer>
-    <div className= "ContainerPrincipal">
-            <p color='white'>PENE PELUO</p>
-    </div>
-</footer>
+        <div className='ContainerPrincipal'>
+          <div>
+          <Paragraph className='Descripcion'>Universidad de Santander - Sistemas de información </Paragraph>
+          </div>
+          <div>
+          <Paragraph className='Descripcion'>Todos los derechos reservados © 2016 </Paragraph>
+          </div>
+          <div>
+          <Paragraph className='Descripcion'>Institución de Educación Superior sujeta a la inspección y vigilancia del Ministerio de Educación Nacional </Paragraph>
+          </div>  
+        </div>
     );
   };
   
