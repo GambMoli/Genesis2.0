2aca7a592058b628d5b4ed85a969e51a1ad334e3
Juan Torres <jstr234567[200~@~gmail.com>
2024-09-27 16:27:41 -0500

    feat: Empezando a hacer el modulo espacios

---
+++ src/Core/Components/InformacionReservas/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const BotonesModulos: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+  };
--- src/Modules/Espacios/index.tsx
+++ src/Modules/Espacios/index.tsx
@@ -1,22 +1,17 @@
+import { PlusOutlined } from '@ant-design/icons';
 import React from 'react';
-
-
-
-
-
-export const Espacios: React.FC = () => {
-  return (
-    <>  
+import { Button } from "antd";
+import './StyleEspacios.css'
+export const Espacios: React.FC = () => (
+  <>
     <div className='MainContainer'>
+      <div className='infoReserva'>
         <h1>PEPEPEP</h1>
+        <div className='BotonAgregar'>
+          <Button><PlusOutlined /></Button>
+        </div>
+      </div>
     </div>
-     
 
-      
-    </>
-
-
-
-
-  );
-};
+  </>
+);
