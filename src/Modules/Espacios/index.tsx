4b989d2ea7eb00668c9558aa688c5e5c588244ef
RozoHyper <jstr234567@gmail.com>
2024-09-27 23:10:18 -0500

    fix: arreglo

--- src/Modules/Espacios/index.tsx
+++ src/Modules/Espacios/index.tsx
@@ -1,17 +1,42 @@
 import { PlusOutlined } from '@ant-design/icons';
-import React from 'react';
-import { Button } from "antd";
+import React, { useState } from 'react';
+import { Button,Modal, Typography } from "antd";
 import './StyleEspacios.css'
-export const Espacios: React.FC = () => (
-  <>
-    <div className='MainContainer'>
-      <div className='infoReserva'>
-        <h1>PEPEPEP</h1>
-        <div className='BotonAgregar'>
-          <Button><PlusOutlined /></Button>
-        </div>
-      </div>
-    </div>
+const {Title,Paragraph}=Typography;
 
-  </>
-);
+export const Espacios: React.FC = () => {
+  const[isModalOpen,SetModalOpen]=useState(false);
+  const showModal = () => {
+    SetModalOpen(true);
+  };
+
+  const handleOk = () => {
+    SetModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    SetModalOpen(false);
+  };
+
+  return (
+    <>
+      <div className='MainContainer'>
+        <div className='ModulosDeInformacion'>
+          <Title>PEPEPEP</Title>
+        </div>
+
+      
+      <div className='BotonAgregar'>
+        <Paragraph className='TituloDeReserva'>Reservar: </Paragraph><Button className='Boton' onClick={showModal}>
+        <PlusOutlined />
+        </Button>
+      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+      </div>
+      </div>
+    </>
+  );
+};
