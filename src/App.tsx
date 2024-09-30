c8931539797a6b36d8b18eaaf1125ebc6c9db260
luisverlet <luisverlet@gmail.com>
2024-09-30 10:55:29 -0500

    libreria

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Login, Home } from './Modules';
+import { Login, Home, Biblioteca } from './Modules';
 import { Header, Footer} from './Core/Components';
 
 const App: React.FC = () => {
@@ -11,6 +11,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/footer" element={<Footer />} />
+        <Route path="/Biblioteca" element={<Biblioteca />} />
       </Routes>
     </Router>
   );
