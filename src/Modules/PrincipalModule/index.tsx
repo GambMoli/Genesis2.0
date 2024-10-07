1e28c741c118ccaabdf8d6b234c92a6d4ca6042e
GambMoli <g.molina@nelumbo.com.co>
2024-10-07 09:47:57 -0500

    feat: adding outes

--- src/Modules/PrincipalModule/index.tsx
+++ src/Modules/PrincipalModule/index.tsx
@@ -28,10 +28,10 @@ export const Home: React.FC = () => {
 
             <Row className="botonesInferior" justify="start"> {/* Gutter reducido */}
               <Col span={12}>
-                <BotonesModulos icon={<MedicineBoxOutlined className="iconsCustom" />} text="Excusas médicas" onClick={() => ''} />
+                <BotonesModulos icon={<MedicineBoxOutlined className="iconsCustom" />} text="Excusas médicas" onClick={() => navigate('/Excusas')} />
               </Col>
               <Col span={12}>
-                <BotonesModulos icon={<SearchOutlined className="iconsCustom" />} text="Pasantías" onClick={() => ''} />
+                <BotonesModulos icon={<SearchOutlined className="iconsCustom" />} text="Pasantías" onClick={() => navigate('/Pasantias')} />
               </Col>
             </Row>
           </Row>
